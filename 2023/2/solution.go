20279a93c6db6f3511335ad024b1b95b9abd2011
Josh Black-Star <23347017+jdblackstar@users.noreply.github.com>
2023-12-06 15:08:12 -0800

    q2p2

--- 2023/2/solution.go
+++ 2023/2/solution.go
@@ -45,15 +45,15 @@ func openFile(filePath string) *os.File {
 	return file
 }
 
-type Game struct {
+type GameMaximum struct {
 	ID    int
-	Red   int
-	Green int
-	Blue  int
+	GameMaxRed   int
+	GameMaxGreen int
+	GameMaxBlue  int
 }
 
-func parseGame(gameStr string) Game {
-	game := Game{}
+func parseGameMaximum(gameStr string) GameMaximum {
+	game := GameMaximum{}
 	// split the string into game number and rounds on colon
 	parts := strings.Split(gameStr, ": ")
 	game.ID, _ = strconv.Atoi(strings.TrimPrefix(parts[0], "Game "))
@@ -93,17 +93,17 @@ func parseGame(gameStr string) Game {
 		}
 	}
 
-	game.Red = maxRed
-	game.Green = maxGreen
-	game.Blue = maxBlue
+	game.GameMaxRed = maxRed
+	game.GameMaxGreen = maxGreen
+	game.GameMaxBlue = maxBlue
 
 	return game
 }
 
-func processGames(games []Game) int {
+func processGames(games []GameMaximum) int {
 	var sumOfValidGameIDs int
 	for _, game := range games {
-		if game.Red <= 12 && game.Green <= 13 && game.Blue <= 14 {
+		if game.GameMaxRed <= 12 && game.GameMaxGreen <= 13 && game.GameMaxBlue <= 14 {
 			sumOfValidGameIDs += game.ID
 		}
 	}
@@ -116,10 +116,10 @@ func part_1() {
 	
 
 	scanner := bufio.NewScanner(file)
-	var games []Game
+	var games []GameMaximum
 	for scanner.Scan() {
 		line := scanner.Text()
-		game := parseGame(line)
+		game := parseGameMaximum(line)
 		games = append(games, game)
 	}
 
@@ -127,8 +127,7 @@ func part_1() {
 		log.Fatal(err)
 	}
 
-	sumOfValidGameIDs := processGames(games)
-	fmt.Println(sumOfValidGameIDs)
+	fmt.Println(processGames(games))
 }
 
 /*
@@ -154,9 +153,87 @@ The power of a set of cubes is equal to the numbers of red, green, and blue cube
 For each game, find the minimum set of cubes that must have been present. What is the sum of the power of these sets?
 */
 
+type Round struct {
+	Red int
+	Green int
+	Blue int
+}
+
+type Game struct {
+	ID int
+	Rounds []Round
+}
+
+func processMinimumCubes(games []Game) int {
+	var totalPower int
+	for _, game := range games {
+		maxRed, maxGreen, maxBlue := 0, 0, 0
+		for _, round := range game.Rounds {
+			if round.Red > maxRed {
+				maxRed = round.Red
+			}
+			if round.Green > maxGreen {
+				maxGreen = round.Green
+			}
+			if round.Blue > maxBlue {
+				maxBlue = round.Blue
+			}
+		}
+		power := maxRed * maxGreen * maxBlue
+		totalPower += power
+	}
+	return totalPower
+}
+
+func parseGame(gameStr string) Game {
+	game := Game{}
+	// split the string into game number and rounds on colon
+	parts := strings.Split(gameStr, ": ")
+	game.ID, _ = strconv.Atoi(strings.TrimPrefix(parts[0], "Game "))
+	// split remaining string on semi-colons, these represent rounds
+	rounds := strings.Split(parts[1], "; ")
+
+	for _, round := range rounds {
+		// split the rounds into color pairs
+		// [2 blue], [3 red], [3 green]
+		// strings.Split() removes the characters that are passed in
+		pairs := strings.Split(round, ", ")
+		round := Round{}
+		for _, pair := range pairs {
+			// split the pair into color and count
+			colorCount := strings.Split(pair, " ")
+			count, _ := strconv.Atoi(colorCount[0])
+			color := colorCount[1]
+
+			// update the count for the color in the round
+			switch color {
+			case "red":
+				round.Red = count
+			case "green":
+				round.Green = count
+			case "blue":
+				round.Blue = count
+			}
+		}
+		game.Rounds = append(game.Rounds, round)
+	}
+
+	return game
+}
+
 func part_2() {
 	file := openFile("2023/2/input.txt")
 	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var games []Game
+	for scanner.Scan() {
+		line := scanner.Text()
+		game := parseGame(line)
+		games = append(games, game)
+	}
+
+	fmt.Println(processMinimumCubes(games))
 }
 
 func main() {
