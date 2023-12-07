8e3677ff4b70cbaafa9f287f56058250e24d2075
Josh Black-Star <23347017+jdblackstar@users.noreply.github.com>
2023-12-07 14:31:54 -0800

    broke part 1.... but got part 2 right

--- 2023/3/solution.go
+++ 2023/3/solution.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	helpers "github.com/jdblackstar/advent_of_code"
-	"strconv"
 	"unicode"
 )
 
@@ -64,41 +63,75 @@ func identifyNumbers(filepath string) map[Coordinate]int {
 				number = number*10 + int(char-'0')
 			} else {
 				if number != 0 {
-					coord := Coordinate{X: startX, Y: y}
-					numbers[coord] = number
-					fmt.Printf("Identified number %d at (%d, %d)\n", number, startX, y) // print the identified number
+					numLength := x - startX
+					for i := 0; i < numLength; i++ {
+						coord := Coordinate{X: startX + i, Y: y}
+						numbers[coord] = number
+					}
+					fmt.Printf("Identified number %d at (%d, %d) to (%d, %d)\n", number, startX, y, startX+numLength-1, y) // print the identified number
 					number = 0
 				}
 			}
 		}
 		if number != 0 {
-			coord := Coordinate{X: startX, Y: y}
-			numbers[coord] = number
-			fmt.Printf("Identified number %d at (%d, %d)\n", number, startX, y) // print the identified number
+			numLength := len(line) - startX
+			for i := 0; i < numLength; i++ {
+				coord := Coordinate{X: startX + i, Y: y}
+				numbers[coord] = number
+			}
+			fmt.Printf("Identified number %d at (%d, %d) to (%d, %d)\n", number, startX, y, startX+numLength-1, y) // print the identified number
 		}
 		y++
 	}
 	return numbers
 }
 
-func checkAroundNumber(coord Coordinate, numLength int, symbols map[Coordinate]rune) bool {
+func checkAroundNumber(coord Coordinate, symbols map[Coordinate]rune) bool {
 	directions := []Coordinate{
 		{-1, -1}, {0, -1}, {1, -1},
 		{-1, 0}, {1, 0},
 		{-1, 1}, {0, 1}, {1, 1},
 	}
 
-	for i := 0; i < numLength; i++ {
-		currentCoord := Coordinate{X: coord.X + i, Y: coord.Y}
-		for _, direction := range directions {
-			adjacentCoord := Coordinate{X: currentCoord.X + direction.X, Y: currentCoord.Y + direction.Y}
-			if _, ok := symbols[adjacentCoord]; ok {
-				return true
-			}
+	for _, direction := range directions {
+		adjacentCoord := Coordinate{X: coord.X + direction.X, Y: coord.Y + direction.Y}
+		if _, ok := symbols[adjacentCoord]; ok {
+			return true
 		}
 	}
 
 	return false
+}
+
+func findNumberPairsNearGears(gears map[Coordinate]rune, numbers map[Coordinate]int) [][]int {
+	numberPairs := [][]int{}
+
+	directions := []Coordinate{
+		{-1, -1}, {0, -1}, {1, -1},
+		{-1, 0}, {1, 0},
+		{-1, 1}, {0, 1}, {1, 1},
+	}
+
+	for gear := range gears {
+		adjacentNumbers := make(map[int]bool)
+		for _, direction := range directions {
+			adjacentCoord := Coordinate{X: gear.X + direction.X, Y: gear.Y + direction.Y}
+			if num, ok := numbers[adjacentCoord]; ok {
+				adjacentNumbers[num] = true
+			}
+		}
+		if len(adjacentNumbers) == 2 {
+			pair := make([]int, 0, 2)
+			for num := range adjacentNumbers {
+				pair = append(pair, num)
+			}
+			numberPairs = append(numberPairs, pair)
+			gearRatio := pair[0] * pair[1]
+			fmt.Printf("Identified number pair %d and %d with gear ratio %d\n", pair[0], pair[1], gearRatio) // print the number pair and gear ratio
+		}
+	}
+
+	return numberPairs
 }
 
 func getPossibleSymbols(filepath string) map[Coordinate]rune {
@@ -123,16 +156,39 @@ func getPossibleSymbols(filepath string) map[Coordinate]rune {
 	return symbols
 }
 
+func findGears(filepath string) map[Coordinate]rune {
+	file := helpers.OpenFile(filepath)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	gears := make(map[Coordinate]rune)
+	y := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		for x, char := range line {
+			if char == '*' {
+				coord := Coordinate{X: x, Y: y}
+				gears[coord] = char
+				fmt.Printf("Identified gear at (%d, %d)\n", x, y) // print the coordinates of the gear
+			}
+		}
+		y++
+	}
+	return gears
+}
+
 func part_1() {
 	filepath := "3/input.txt"
 	symbols := getPossibleSymbols(filepath)
 	numbers := identifyNumbers(filepath)
 
 	sum := 0
+	addedNumbers := make(map[int]bool)
 	for coord, num := range numbers {
-		numLength := len(strconv.Itoa(num)) // calculate the length of the number
-		if checkAroundNumber(coord, numLength, symbols) {
+		if _, alreadyAdded := addedNumbers[num]; !alreadyAdded && checkAroundNumber(coord, symbols) {
 			sum += num
+			addedNumbers[num] = true
 		}
 	}
 
@@ -140,7 +196,19 @@ func part_1() {
 }
 
 func part_2() {
+	filepath := "3/input.txt"
+	gears := findGears(filepath)
+	numbers := identifyNumbers(filepath)
 
+	numberPairs := findNumberPairsNearGears(gears, numbers)
+
+	sum := 0
+	for _, pair := range numberPairs {
+		gearRatio := pair[0] * pair[1]
+		sum += gearRatio
+	}
+
+	fmt.Println(sum)
 }
 
 func main() {
