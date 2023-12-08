a3b8a416011fd036a27e11cca2a715fbb30f6782
Josh Black-Star <23347017+jdblackstar@users.noreply.github.com>
2023-12-07 21:58:25 -0800

    day 4 part 1 done

--- 2023/4/solution.go
+++ 2023/4/solution.go
@@ -1,6 +1,76 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
 	helpers "github.com/jdblackstar/advent-of-code"
 )
 
+func splitNumbers(input string) ([]int, []int) {
+	splitInput := strings.Split(input, "|")
+	winningNumbersStr := strings.Fields(splitInput[0])
+	ourNumbersStr := strings.Fields(splitInput[1])
+
+	winningNumbers := make([]int, len(winningNumbersStr))
+	ourNumbers := make([]int, len(ourNumbersStr))
+
+	for i, numStr := range winningNumbersStr {
+		num, _ := strconv.Atoi(numStr)
+		winningNumbers[i] = num
+	}
+
+	for i, numStr := range ourNumbersStr {
+		num, _ := strconv.Atoi(numStr)
+		ourNumbers[i] = num
+	}
+
+	return winningNumbers, ourNumbers
+}
+
+func totalPointsPerCard(winningNumbers []int, ourNumbers []int) int {
+	points := 0
+	matches := 0
+
+	for _, ourNumber := range ourNumbers {
+		for _, winningNumber := range winningNumbers {
+			if ourNumber == winningNumber {
+				matches++
+				break
+			}
+		}
+	}
+
+	if matches > 0 {
+		points = 1 << (matches - 1)
+	}
+
+	return points
+}
+
+func part_1(filepath string) int {
+	file := helpers.OpenFile(filepath)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	totalPoints := 0
+
+	for scanner.Scan() {
+		card := scanner.Text()
+		winningNumbers, ourNumbers := splitNumbers(card)
+		totalPoints += totalPointsPerCard(winningNumbers, ourNumbers)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return totalPoints
+}
+
+func main() {
+	fmt.Println("Solution for part 1: ", part_1("4/input.txt"))
+}
