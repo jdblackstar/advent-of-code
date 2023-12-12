eca6a062aeb2ee474d0beb11406f7f1635ed9aa6
Josh Black-Star <23347017+jdblackstar@users.noreply.github.com>
2023-12-12 14:55:28 -0800

    part one done i fell into the noob trap yikes

--- 2023/5/solution.go
+++ 2023/5/solution.go
@@ -3,9 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
-	"os"
 )
 
 /*
@@ -53,8 +53,6 @@ for step in map[2]:
 then repeat for next map until we get to location
 */
 
-
-
 func getSeeds(fileName string) ([]int, error) {
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -79,10 +77,73 @@ func getSeeds(fileName string) ([]int, error) {
 	return seeds, nil
 }
 
+func parseMapping(fileContent string) []map[string][][3]int {
+	maps := make([]map[string][][3]int, 0)
+	lines := strings.Split(fileContent, "\n")[2:] // start from line 3
+	mapName := ""
+	currentMap := make(map[string][][3]int)
+	for _, line := range lines {
+		if line == "" {
+			maps = append(maps, currentMap)
+			currentMap = make(map[string][][3]int)
+			continue
+		}
+		if strings.Contains(line, " map:") {
+			mapName = strings.Split(line, " map:")[0]
+		} else {
+			numsStr := strings.Split(line, " ")
+			destination, _ := strconv.Atoi(numsStr[0])
+			source, _ := strconv.Atoi(numsStr[1])
+			length, _ := strconv.Atoi(numsStr[2])
+			currentMap[mapName] = append(currentMap[mapName], [3]int{source, destination, length})
+		}
+	}
+	maps = append(maps, currentMap)
+	return maps
+}
 
 func main() {
-	filepath := "5/small.txt"
+	filepath := "5/input.txt"
 	seeds, _ := getSeeds(filepath)
 
-	fmt.Println(seeds)
+	file, _ := os.Open(filepath)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var txtlines []string
+
+	for scanner.Scan() {
+		txtlines = append(txtlines, scanner.Text())
+	}
+
+	fileContent := strings.Join(txtlines, "\n")
+
+	maps := parseMapping(fileContent)
+
+	mappingOrder := []string{"seed-to-soil", "soil-to-fertilizer", "fertilizer-to-water", "water-to-light", "light-to-temperature", "temperature-to-humidity", "humidity-to-location"}
+
+	newSeeds := make([]int, len(seeds))
+	minSeed := int(^uint(0) >> 1) // set to max int value
+	for i, seed := range seeds {
+		fmt.Printf("Original seed: %d\n", seed)
+		for _, mapName := range mappingOrder {
+			for _, currentMap := range maps {
+				for _, mapping := range currentMap[mapName] {
+					if mapping[0] <= seed && seed <= mapping[0]+mapping[2] {
+						seed = seed - (mapping[0] - mapping[1])
+						fmt.Printf("Mapping through %s, seed becomes %d\n", mapName, seed)
+						break
+					}
+				}
+			}
+		}
+		newSeeds[i] = seed
+		fmt.Printf("Final value: %d\n", seed)
+		if seed < minSeed {
+			minSeed = seed
+		}
+	}
+	fmt.Println("New seeds:", newSeeds)
+	fmt.Println("Minimum seed:", minSeed)
 }
