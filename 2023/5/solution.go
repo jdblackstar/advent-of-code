05e762d42e0a9b0b9b1b6a4adbc8cf05123d6d97
Josh Black-Star <23347017+jdblackstar@users.noreply.github.com>
2023-12-15 10:22:08 -0800

    not quite there, but saving progress

--- 2023/5/solution.go
+++ 2023/5/solution.go
@@ -77,6 +77,31 @@ func getSeeds(fileName string) ([]int, error) {
 	return seeds, nil
 }
 
+func getRangeOfSeeds(filepath string) ([][]int, error) {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan() // skip the first line
+	seedsLine := scanner.Text()
+
+	seedsStr := strings.Split(seedsLine, " ")[1:] // skip the "seeds:" part
+	ranges := make([][]int, 0)
+	totalCount := 0
+	for i := 0; i < len(seedsStr); i += 2 {
+		start, _ := strconv.Atoi(seedsStr[i])
+		length, _ := strconv.Atoi(seedsStr[i+1])
+		ranges = append(ranges, []int{start, start + length - 1})
+		totalCount += length
+	}
+
+	fmt.Printf("Total count of seeds to be examined: %d\n", totalCount)
+	return ranges, nil
+}
+
 func parseMapping(fileContent string) []map[string][][3]int {
 	maps := make([]map[string][][3]int, 0)
 	lines := strings.Split(fileContent, "\n")[2:] // start from line 3
@@ -102,10 +127,28 @@ func parseMapping(fileContent string) []map[string][][3]int {
 	return maps
 }
 
-func main() {
-	filepath := "5/input.txt"
-	seeds, _ := getSeeds(filepath)
+/*
+example is to place fertilizer (f) 10 into a corresponding water
+this is how we avoid the noob trap of creating every possible combination
 
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+If 53 <= f <= (53 + 8), then w = f - (53 - 49)
+If 11 <= f <= (11 + 42), then w = f - (11 - 0)
+If 0 <= f <= (0 + 7), then w = f - (0 - 42)
+If 7 <= f <= (7 + 4), then w = f - (7 - 57)
+
+The pattern is essentially:
+if map[1] <= input <= (map[1] + map[2]), then output = input - (map[1] - map[0])
+
+In this scenario, the last line executes properly, so if you enter 10, the output would be 10 - (7 - 57) or 60
+*/
+
+func findLowestSeed(filepath string, getSeedsFunc interface{}) int {
 	file, _ := os.Open(filepath)
 	defer file.Close()
 
@@ -131,27 +174,57 @@ func main() {
 		"humidity-to-location",
 	}
 
-	newSeeds := make([]int, len(seeds))
 	minSeed := int(^uint(0) >> 1) // set to max int value
-	for i, seed := range seeds {
-		fmt.Printf("Original seed: %d\n", seed)
-		for _, mapName := range mappingOrder {
-			for _, currentMap := range maps {
-				for _, mapping := range currentMap[mapName] {
-					if mapping[0] <= seed && seed <= mapping[0]+mapping[2] {
-						seed = seed - (mapping[0] - mapping[1])
-						fmt.Printf("Mapping through %s, seed becomes %d\n", mapName, seed)
-						break
-					}
+
+	switch getSeeds := getSeedsFunc.(type) {
+	case func(string) ([]int, error):
+		seeds, _ := getSeeds(filepath)
+		for _, seed := range seeds {
+			fmt.Printf("Original seed: %d\n", seed)
+			seed = mapSeed(seed, maps, mappingOrder)
+			fmt.Printf("Final value: %d\n", seed)
+			if seed < minSeed {
+				minSeed = seed
+			}
+		}
+	case func(string) ([][]int, error):
+		ranges, _ := getSeeds(filepath)
+		for _, rng := range ranges {
+			for seed := rng[0]; seed <= rng[1]; seed++ {
+				fmt.Printf("Original seed: %d\n", seed)
+				seed = mapSeed(seed, maps, mappingOrder)
+				fmt.Printf("Final value: %d\n", seed)
+				if seed < minSeed {
+					minSeed = seed
 				}
 			}
 		}
-		newSeeds[i] = seed
-		fmt.Printf("Final value: %d\n", seed)
-		if seed < minSeed {
-			minSeed = seed
+	}
+
+	return minSeed
+}
+
+func mapSeed(seed int, maps []map[string][][3]int, mappingOrder []string) int {
+	for _, mapName := range mappingOrder {
+		matched := false
+		for _, currentMap := range maps {
+			for _, mapping := range currentMap[mapName] {
+				if mapping[0] <= seed && seed < mapping[0]+mapping[2] {
+					seed = seed - (mapping[0] - mapping[1])
+					fmt.Printf("Mapping through %s, seed becomes %d\n", mapName, seed)
+					matched = true
+				}
+			}
+		}
+		if !matched {
+			fmt.Printf("No match in %s map, seed stays %d\n", mapName, seed)
 		}
 	}
-	fmt.Println("New seeds:", newSeeds)
-	fmt.Println("Minimum seed:", minSeed)
+	return seed
+}
+
+func main() {
+	filepath := "5/small.txt"
+	fmt.Println("Solution for part one: ", findLowestSeed(filepath, getSeeds))
+	fmt.Println("Solution for part two: ", findLowestSeed(filepath, getRangeOfSeeds))
 }
